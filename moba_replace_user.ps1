688669ce62d41899a618e945c4a6e3ad325aad61
Stan Rouyer <stanislas.rouyer@gmail.com>
2022-04-28 21:29:27 +0200

    Update moba_replace_user.ps1

--- moba_replace_user.ps1
+++ moba_replace_user.ps1
@@ -3,15 +3,14 @@
 # Il récupère ce numéro, l'indique à l'utilisateur, et lui demande un autre numéro
 # Ce dernier numéro est remplacé par celui entré par l'utilisateur
 
-Clear
+Clear-Host
 
 # Récupération des lignes contenant les caractères "%%"
 $file = Get-Content .\moba.txt
-
 foreach ($line in $file){if ($line -like "*%%*") {$line | out-file -FilePath .\moba2.txt -Append}}
 
 # Remplacement de chaque "%" par des retours à la ligne
-(Get-Content .\moba2.txt | Select -First 1) -replace '%',"`r`n" | Set-Content -Path moba3.txt
+(Get-Content .\moba2.txt | Select-Object -First 1) -replace '%',"`r`n" | Set-Content -Path moba3.txt
 
 # Récupération des lignes commençant par "e", et affectation dans une variable
 foreach ($line in (Get-Content -Path .\moba3.txt)) {if ($line.StartsWith('e')){$line2=$line}}
@@ -20,7 +19,7 @@ foreach ($line in (Get-Content -Path .\moba3.txt)) {if ($line.StartsWith('e')){$
 $olduser=($line2 -replace 'e','')
 
 # Prompt du nouvel utilisateur
-echo "Le logon suivant va être remplacé : $olduser"
+Write-Output "Le logon suivant va être remplacé : $olduser"
 $User=Read-Host -Prompt "Quel logon mettre à la place ?"
 
 # Remplacement de l'utilisateur dans le fichier de configuration
